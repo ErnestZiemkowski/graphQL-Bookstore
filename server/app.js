270fac04fa7644c5b525948f1e1f2b14f9131626
ErnestZiemkowski <ernest.ziemkowski@gmail.com>
2018-05-29 22:18:42 +0200

    Node models

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://ernest:dupadupa@ds139890.mlab.com:39890/bookshelf');
+mongoose.connection.once('open', () => {
+    console.log('connected to DataBase');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
